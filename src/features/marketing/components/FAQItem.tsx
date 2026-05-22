086ff34a84db7e6961b327fba9d347b648d5b7f1
kofoworola_shonuyi <skofoworola3@gmail.com>
2026-05-22 21:38:03 +0100

    feat: implement marketing landing page  FAQ section and configure husky pre-commit hooks

---
+++ src/features/marketing/components/FAQItem.tsx
@@ -0,0 +1,29 @@
+import { ChevronDown, ChevronUp } from "lucide-react";
+
+type FAQItemProps = {
+  question: string;
+  answer: string;
+  isOpen: boolean;
+  onToggle: () => void;
+};
+
+export const FAQItem = ({
+  question,
+  answer,
+  isOpen,
+  onToggle,
+}: FAQItemProps) => {
+  return (
+    <div className="flex flex-col rounded-xl gap-4.5  p-4.5 border-marketing-border border bg-marketing-card">
+      <button onClick={onToggle} className="flex justify-between">
+        <h3 className="font-semibold text-xl text-marketing-muted">
+          {question}
+        </h3>
+        <span>{isOpen ? <ChevronUp /> : <ChevronDown />}</span>
+      </button>
+      {isOpen && (
+        <p className="text-xl font-medium text-marketing-muted">{answer}</p>
+      )}
+    </div>
+  );
+};
